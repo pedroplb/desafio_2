2cffced894d736c1a34e3d9a608e351f6b759720
pedroplb <91922496+pedroplb@users.noreply.github.com>
2021-10-16 13:19:23 -0300

    segundo commit

--- correcao.py
+++ correcao.py
@@ -3,7 +3,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host='127.0.0.1',
     user='root',
-    password='minuano',
+    password='teste',
     database='demodb'
 )
 cursor = conn.cursor()
